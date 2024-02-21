d5b69ea2b8112d6cafcbd379cd0de80fe3af1f85
Andrew Speciale <114184256+liquidz00@users.noreply.github.com>
2024-02-21 15:26:52 -0500

    Added default UI parameters

---
+++ ui-config.py
@@ -0,0 +1,11 @@
+import os
+
+BASE = os.path.abspath(os.path.dirname(__file__))
+FONTS = os.path.join(BASE, "fonts")
+
+# Default UI Configurations
+HEADER_TEXT = "Default Header Text"
+FOOTER_TEXT = "Default Footer Text"
+FONT_NAME = "Assistant"
+FONT_REGULAR_PATH = os.path.join(FONTS, "Assistant-Regular.ttf")
+FONT_BOLD_PATH = os.path.join(FONTS, "Assistant-Bold.ttf")
