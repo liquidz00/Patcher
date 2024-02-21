5c6d1c6e879eea2bcd9b2374ea37545f9c81c222
Andrew Speciale <114184256+liquidz00@users.noreply.github.com>
2024-02-21 17:10:24 -0500

    Removed org-specific info from ui_config.py

--- ui_config.py
+++ ui_config.py
@@ -4,8 +4,8 @@ BASE = os.path.abspath(os.path.dirname(__file__))
 FONTS = os.path.join(BASE, "fonts")
 
 # Default UI Configurations
-HEADER_TEXT = "Advisor360° Mac Patch Report"
-FOOTER_TEXT = "Default Footer Text"
+HEADER_TEXT = "Default header text"
+FOOTER_TEXT = "Default footer text"
 FONT_NAME = "Assistant"
 FONT_REGULAR_PATH = os.path.join(FONTS, "Assistant-Regular.ttf")
 FONT_BOLD_PATH = os.path.join(FONTS, "Assistant-Bold.ttf")
