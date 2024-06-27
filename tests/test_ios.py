a33a54d4a6f33a7613a558cd157df34ca00d1d86
Andrew Speciale (@liquidz00) <aspeciale88@gmail.com>
2024-06-27 16:55:09 -0400

    Refactor unit tests to match OOP project structure

--- tests/test_ios.py
+++ tests/test_ios.py
@@ -4,20 +4,35 @@ import subprocess
 import json
 from unittest.mock import patch, MagicMock
 from aioresponses import aioresponses
-from src import utils
-from conftest import jamf_url, headers
+from src.client.api_client import ApiClient
+from src.utils import calculate_ios_on_latest
+
+
+@pytest.fixture
+def api_client(config_manager):
+    return ApiClient(config=config_manager)
 
 
 # Test valid response - iOS device IDs
 @pytest.mark.asyncio
-async def test_get_device_ids_valid(mock_ios_device_id_list_response, mock_env_vars):
+async def test_get_device_ids_valid(
+    api_client,
+    mock_ios_device_id_list_response,
+    mock_env_vars,
+    mock_api_integration_response,
+):
     with aioresponses() as m:
         m.get(
-            url=f"{jamf_url}/api/v2/mobile-devices",
+            url="https://mocked.url/api/v2/mobile-devices",
             payload=mock_ios_device_id_list_response,
-            headers=headers,
+            headers={"Accept": "application/json"},
         )
-        devices = await utils.get_device_ids()
+        m.get(
+            "https://mocked.url/api/v1/api-integrations",
+            payload=mock_api_integration_response,
+            headers={"Accept": "application/json"},
+        )
+        devices = await api_client.get_device_ids()
 
         assert len(devices) == len(mock_ios_device_id_list_response)
         assert devices[0] == mock_ios_device_id_list_response.get("results")[0]["id"]
@@ -25,86 +40,108 @@ async def test_get_device_ids_valid(mock_ios_device_id_list_response, mock_env_v
 
 # Test invalid response - iOS device IDs
 @pytest.mark.asyncio
-async def test_get_device_ids_invalid(mock_env_vars):
+async def test_get_device_ids_invalid(
+    api_client, mock_api_integration_response, mock_env_vars
+):
     with aioresponses() as m:
         m.get(
-            url=f"{jamf_url}/api/v2/mobile-devices",
+            url="https://mocked.url/api/v2/mobile-devices",
             payload={"invalid": "response"},
-            headers=headers,
+            headers={"Accept": "application/json"},
         )
-        devices = await utils.get_device_ids()
+        m.get(
+            "https://mocked.url/api/v1/api-integrations",
+            payload=mock_api_integration_response,
+            headers={"Accept": "application/json"},
+        )
+        devices = await api_client.get_device_ids()
         assert devices is None
 
 
 # Test API error response
 @pytest.mark.asyncio
 async def test_get_device_ids_api_error(
-    mock_ios_device_id_list_response, mock_env_vars
+    api_client,
+    mock_ios_device_id_list_response,
+    mock_env_vars,
+    mock_api_integration_response,
 ):
     with aioresponses() as m:
         m.get(
-            url=f"{jamf_url}/api/v2/mobile-devices",
+            url="https://mocked.url/api/v2/mobile-devices",
             payload=mock_ios_device_id_list_response,
-            headers=headers,
+            headers={"Accept": "application/json"},
             status=401,
         )
-        devices = await utils.get_device_ids()
+        m.get(
+            "https://mocked.url/api/v1/api-integrations",
+            payload=mock_api_integration_response,
+            headers={"Accept": "application/json"},
+        )
+        devices = await api_client.get_device_ids()
         assert devices is None
         assert pytest.raises(aiohttp.ClientError)
 
 
 # Test valid response - Getting iOS Versions
 @pytest.mark.asyncio
-async def test_get_ios_versions_valid(mock_ios_detail_response, mock_env_vars):
+async def test_get_ios_versions_valid(
+    api_client, mock_ios_detail_response, mock_env_vars, mock_api_integration_response
+):
     device_ids = [1]
 
     with aioresponses() as m:
         for device_id in device_ids:
             m.get(
-                url=f"{jamf_url}/api/v2/mobile-devices/{device_id}/detail",
+                url=f"https://mocked.url/api/v2/mobile-devices/{device_id}/detail",
                 payload=mock_ios_detail_response,
-                headers=headers,
+                headers={"Accept": "application/json"},
+            )
+            m.get(
+                "https://mocked.url/api/v1/api-integrations",
+                payload=mock_api_integration_response,
+                headers={"Accept": "application/json"},
             )
 
-        fetched_devices = await utils.get_device_os_versions(device_ids)
+        fetched_devices = await api_client.get_device_os_versions(device_ids)
 
     assert fetched_devices[0].get("SN") == mock_ios_detail_response.get("serialNumber")
     assert fetched_devices[0].get("OS") == mock_ios_detail_response.get("osVersion")
 
 
-# Test passing empty list when obtaining iOS Versions
-@pytest.mark.asyncio
-async def test_get_ios_versions_empty_list():
-    fetched_devices = await utils.get_device_os_versions([])
-    assert fetched_devices is None
-
-
 # Test unauthorized API response
 @pytest.mark.asyncio
-async def test_get_ios_version_api_error(mock_ios_detail_response):
+async def test_get_ios_version_api_error(
+    api_client, mock_ios_detail_response, mock_api_integration_response
+):
     device_ids = [1]
 
     with aioresponses() as m:
         for device_id in device_ids:
             m.get(
-                url=f"{jamf_url}/api/v2/mobile-devices/{device_id}/detail",
+                url="https://mocked.url/api/v2/mobile-devices/{device_id}/detail",
                 payload=mock_ios_detail_response,
-                headers=headers,
+                headers={"Accept": "application/json"},
                 status=401,
             )
+            m.get(
+                "https://mocked.url/api/v1/api-integrations",
+                payload=mock_api_integration_response,
+                headers={"Accept": "application/json"},
+            )
 
-        await utils.get_device_os_versions(device_ids=device_ids)
+        await api_client.get_device_os_versions(device_ids=device_ids)
 
     assert pytest.raises(aiohttp.ClientError)
 
 
 # Test SOFA functionality with valid response
 @patch("subprocess.run")
-def test_sofa_valid(mock_run, mock_sofa_response):
+def test_sofa_valid(mock_run, api_client, mock_sofa_response):
     mock_run.return_value = MagicMock(
         stdout=json.dumps(mock_sofa_response), returncode=0
     )
-    result = utils.get_sofa_feed()
+    result = api_client.get_sofa_feed()
     expected_result = [
         {"OSVersion": "17", "ProductVersion": "17.5.1", "ReleaseDate": "May 20 2024"},
         {"OSVersion": "16", "ProductVersion": "16.7.8", "ReleaseDate": "May 13 2024"},
@@ -113,20 +150,22 @@ def test_sofa_valid(mock_run, mock_sofa_response):
 
 
 # Test subprocess error handling
-@patch("subprocess.run", side_effect=subprocess.CalledProcessError(1, "curl"))
-def test_get_sofa_feed_subprocess_error(mock_run):
-    result = utils.get_sofa_feed()
+@patch("subprocess.run")
+def test_get_sofa_feed_subprocess_error(mock_run, api_client):
+    mock_run.side_effect = subprocess.CalledProcessError(1, "curl")
+    result = api_client.get_sofa_feed()
     assert result is None
     assert pytest.raises(subprocess.CalledProcessError)
 
 
 # Test JSON decoding error handling
 @patch("subprocess.run")
-def test_get_sofa_feed_json_decode_error(mock_run):
+def test_get_sofa_feed_json_decode_error(mock_run, api_client):
     mock_run.return_value = MagicMock(stdout="Invalid JSON", returncode=0)
-    result = utils.get_sofa_feed()
+    result = api_client.get_sofa_feed()
     assert result is None
     assert pytest.raises(json.JSONDecodeError)
+
 
 # Test successful calculation
 @pytest.mark.asyncio
@@ -137,11 +176,19 @@ async def test_calculate_ios_on_latest_success():
         {"DeviceID": "3", "OS": "17.5.1"},
     ]
     latest_versions = [
-        {"OSVersion": "17", "ProductVersion": "17.5.1", "ReleaseDate": "2024-05-20T00:00:00Z"},
-        {"OSVersion": "16", "ProductVersion": "16.7.8", "ReleaseDate": "2024-05-13T00:00:00Z"},
+        {
+            "OSVersion": "17",
+            "ProductVersion": "17.5.1",
+            "ReleaseDate": "2024-05-20T00:00:00Z",
+        },
+        {
+            "OSVersion": "16",
+            "ProductVersion": "16.7.8",
+            "ReleaseDate": "2024-05-13T00:00:00Z",
+        },
     ]
 
-    result = utils.calculate_ios_on_latest(device_versions, latest_versions)
+    result = calculate_ios_on_latest(device_versions, latest_versions)
     expected_result = [
         {
             "software_title": "iOS 17.5.1",
@@ -149,7 +196,7 @@ async def test_calculate_ios_on_latest_success():
             "hosts_patched": 2,
             "missing_patch": 0,
             "completion_percent": 100.0,
-            "total_hosts": 2
+            "total_hosts": 2,
         },
         {
             "software_title": "iOS 16.7.8",
@@ -157,11 +204,12 @@ async def test_calculate_ios_on_latest_success():
             "hosts_patched": 1,
             "missing_patch": 0,
             "completion_percent": 100.0,
-            "total_hosts": 1
-        }
+            "total_hosts": 1,
+        },
     ]
 
     assert result == expected_result
+
 
 # Test no devices on the latest version
 @pytest.mark.asyncio
@@ -171,11 +219,19 @@ async def test_calculate_ios_on_latest_no_devices_on_latest():
         {"DeviceID": "2", "OS": "16.6.0"},
     ]
     latest_versions = [
-        {"OSVersion": "17", "ProductVersion": "17.5.1", "ReleaseDate": "2024-05-20T00:00:00Z"},
-        {"OSVersion": "16", "ProductVersion": "16.7.8", "ReleaseDate": "2024-05-13T00:00:00Z"},
+        {
+            "OSVersion": "17",
+            "ProductVersion": "17.5.1",
+            "ReleaseDate": "2024-05-20T00:00:00Z",
+        },
+        {
+            "OSVersion": "16",
+            "ProductVersion": "16.7.8",
+            "ReleaseDate": "2024-05-13T00:00:00Z",
+        },
     ]
 
-    result = utils.calculate_ios_on_latest(device_versions, latest_versions)
+    result = calculate_ios_on_latest(device_versions, latest_versions)
     expected_result = [
         {
             "software_title": "iOS 17.5.1",
@@ -183,7 +239,7 @@ async def test_calculate_ios_on_latest_no_devices_on_latest():
             "hosts_patched": 0,
             "missing_patch": 1,
             "completion_percent": 0.0,
-            "total_hosts": 1
+            "total_hosts": 1,
         },
         {
             "software_title": "iOS 16.7.8",
@@ -191,11 +247,12 @@ async def test_calculate_ios_on_latest_no_devices_on_latest():
             "hosts_patched": 0,
             "missing_patch": 1,
             "completion_percent": 0.0,
-            "total_hosts": 1
-        }
+            "total_hosts": 1,
+        },
     ]
 
     assert result == expected_result
+
 
 # Test all devices on the latest version
 @pytest.mark.asyncio
@@ -205,10 +262,14 @@ async def test_calculate_ios_on_latest_all_devices_on_latest():
         {"DeviceID": "2", "OS": "17.5.1"},
     ]
     latest_versions = [
-        {"OSVersion": "17", "ProductVersion": "17.5.1", "ReleaseDate": "2024-05-20T00:00:00Z"},
+        {
+            "OSVersion": "17",
+            "ProductVersion": "17.5.1",
+            "ReleaseDate": "2024-05-20T00:00:00Z",
+        },
     ]
 
-    result = utils.calculate_ios_on_latest(device_versions, latest_versions)
+    result = calculate_ios_on_latest(device_versions, latest_versions)
     expected_result = [
         {
             "software_title": "iOS 17.5.1",
@@ -216,11 +277,12 @@ async def test_calculate_ios_on_latest_all_devices_on_latest():
             "hosts_patched": 2,
             "missing_patch": 0,
             "completion_percent": 100.0,
-            "total_hosts": 2
+            "total_hosts": 2,
         }
     ]
 
     assert result == expected_result
+
 
 # Test some devices on the latest version
 @pytest.mark.asyncio
@@ -230,10 +292,14 @@ async def test_calculate_ios_on_latest_some_devices_on_latest():
         {"DeviceID": "2", "OS": "17.4.0"},
     ]
     latest_versions = [
-        {"OSVersion": "17", "ProductVersion": "17.5.1", "ReleaseDate": "2024-05-20T00:00:00Z"},
+        {
+            "OSVersion": "17",
+            "ProductVersion": "17.5.1",
+            "ReleaseDate": "2024-05-20T00:00:00Z",
+        },
     ]
 
-    result = utils.calculate_ios_on_latest(device_versions, latest_versions)
+    result = calculate_ios_on_latest(device_versions, latest_versions)
     expected_result = [
         {
             "software_title": "iOS 17.5.1",
@@ -241,7 +307,7 @@ async def test_calculate_ios_on_latest_some_devices_on_latest():
             "hosts_patched": 1,
             "missing_patch": 1,
             "completion_percent": 50.0,
-            "total_hosts": 2
+            "total_hosts": 2,
         }
     ]
 
