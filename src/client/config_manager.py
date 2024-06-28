e35a0ca2ecfaf8c94eda7163130a5237468c98d5
Andrew Speciale (@liquidz00) <aspeciale88@gmail.com>
2024-06-28 17:21:02 -0400

    Separate UIConfig from CredentialConfig

--- src/client/config_manager.py
+++ src/client/config_manager.py
@@ -1,12 +1,5 @@
 import keyring
 import configparser
-import os
-
-from typing import Dict
-
-SRC_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-ROOT_DIR = os.path.dirname(SRC_DIR)
-FONT_DIR = os.path.join(ROOT_DIR, "fonts")
 
 class ConfigManager:
 
@@ -14,42 +7,11 @@ class ConfigManager:
         self.service_name = service_name
         self.config = configparser.ConfigParser()
 
-        # Paths
-        default_path = os.path.join(ROOT_DIR, "config.ini")
-        user_config_dir = os.path.expanduser("~/Library/Application Support/Patcher")
-        user_config_path = os.path.join(user_config_dir, "config.ini")
-
-        # Ensure directory exists
-        os.makedirs(user_config_dir, exist_ok=True)
-
-        # Load configs
-        self.config.read(default_path)
-
-        # Override with user configuration if available
-        if os.path.exists(user_config_path):
-            self.config.read(user_config_path)
-
     def get_credential(self, key: str) -> str:
         return keyring.get_password(self.service_name, key)
 
     def set_credential(self, key: str, value: str):
         keyring.set_password(self.service_name, key, value)
 
-    def get_ui_config(self) -> Dict:
-        return {
-            "HEADER_TEXT": self.config.get("UI", "HEADER_TEXT"),
-            "FOOTER_TEXT": self.config.get(
-                "UI", "FOOTER_TEXT", fallback="Default footer text"
-            ),
-            "FONT_NAME": self.config.get("UI", "FONT_NAME", fallback="Assistant"),
-            "FONT_REGULAR_PATH": self.config.get(
-                "UI",
-                "FONT_REGULAR_PATH",
-                fallback=os.path.join(FONT_DIR, "Assistant-Regular.ttf"),
-            ),
-            "FONT_BOLD_PATH": self.config.get(
-                "UI",
-                "FONT_BOLD_PATH",
-                fallback=os.path.join(FONT_DIR, "Assistant-Bold.ttf"),
-            ),
-        }
+
+
---
+++ src/client/ui_manager.py
@@ -0,0 +1,51 @@
+import configparser
+import os
+from typing import Dict, AnyStr
+
+SRC_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = os.path.dirname(SRC_DIR)
+FONT_DIR = os.path.join(ROOT_DIR, "fonts")
+
+class UIConfigManager:
+
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.load_ui_config()
+
+    def load_ui_config(self):
+        # Paths
+        default_path = os.path.join(ROOT_DIR, "config.ini")
+        user_config_dir = os.path.expanduser("~/Library/Application Support/Patcher")
+        user_config_path = os.path.join(user_config_dir, "config.ini")
+
+        # Ensure directory exists
+        os.makedirs(user_config_dir, exist_ok=True)
+
+        # Load configs
+        self.config.read(default_path)
+
+        # Override with user configuration if available
+        if os.path.exists(user_config_path):
+            self.config.read(user_config_path)
+
+    def get_ui_config(self) -> Dict:
+        return {
+            "HEADER_TEXT": self.config.get("UI", "HEADER_TEXT"),
+            "FOOTER_TEXT": self.config.get(
+                "UI", "FOOTER_TEXT", fallback="Default footer text"
+            ),
+            "FONT_NAME": self.config.get("UI", "FONT_NAME", fallback="Assistant"),
+            "FONT_REGULAR_PATH": self.config.get(
+                "UI",
+                "FONT_REGULAR_PATH",
+                fallback=os.path.join(FONT_DIR, "Assistant-Regular.ttf"),
+            ),
+            "FONT_BOLD_PATH": self.config.get(
+                "UI",
+                "FONT_BOLD_PATH",
+                fallback=os.path.join(FONT_DIR, "Assistant-Bold.ttf"),
+            ),
+        }
+
+    def get(self, key: AnyStr, fallback: AnyStr = None) -> AnyStr:
+        return self.get_ui_config().get(key, fallback)
