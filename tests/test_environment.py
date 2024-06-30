5d3b1e6a80c747cea31e47eddbd5e1c58aa24d93
Andrew Speciale (@liquidz00) <aspeciale88@gmail.com>
2024-06-30 13:00:11 -0400

    Refactor token validity unit tests to test_environment.py

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,34 +1,8 @@
-import pytest
-from datetime import datetime, timezone
-from unittest.mock import patch, call, ANY
+from datetime import datetime, timezone, timedelta
+from unittest.mock import patch, call, ANY, MagicMock
 from src.client.config_manager import ConfigManager
 from src.client.token_manager import TokenManager
 from src.model.models import AccessToken
-
-
-@pytest.fixture
-def mock_config_manager():
-    with (
-        patch("src.client.config_manager.keyring.get_password") as mock_get_password,
-        patch("src.client.config_manager.keyring.set_password") as mock_set_password,
-    ):
-
-        def side_effect(service_name, key):
-            if key == "TOKEN":
-                return "mocked_token"
-            elif key == "TOKEN_EXPIRATION":
-                return datetime(2030, 1, 1, tzinfo=timezone.utc).isoformat()
-            elif key == "URL":
-                return "https://mocked.url"
-            elif key == "CLIENT_ID":
-                return "mocked_client_id"
-            elif key == "CLIENT_SECRET":
-                return "mocked_client_secret"
-            return None
-
-        mock_get_password.side_effect = side_effect
-        config_manager = ConfigManager(service_name="patcher")
-        yield config_manager
 
 
 @patch.object(ConfigManager, "set_credential")
@@ -46,16 +20,17 @@ def test_update_env(mock_set_credential, config_manager):
     assert mock_set_credential.call_count == 2
 
 
-def test_config_manager_initialization(mock_config_manager):
-    assert mock_config_manager.get_credential("TOKEN") == "mocked_token"
-    assert (
-        mock_config_manager.get_credential("TOKEN_EXPIRATION")
-        == datetime(2030, 1, 1, tzinfo=timezone.utc).isoformat()
-    )
+@patch.object(TokenManager, "check_token_lifetime", return_value=False)
+def test_check_short_lifetime(mock_lifetime_response, short_lived_jamf_client):
+    config = MagicMock(return_value=short_lived_jamf_client)
+    token_manager = TokenManager(config=config)
+    result = token_manager.check_token_lifetime(client=short_lived_jamf_client)
+    assert result is False
+    assert token_manager.token_valid() is False
 
 
 @patch.object(ConfigManager, "get_credential")
-def test_get_credentials(mock_get_credential, mock_config_manager):
+def test_get_credentials(mock_get_credential, config_manager):
     def side_effect(key):
         if key == "CLIENT_ID":
             return "mocked_client_id"
@@ -65,21 +40,21 @@ def test_get_credentials(mock_get_credential, mock_config_manager):
 
     mock_get_credential.side_effect = side_effect
 
-    token_manager = TokenManager(config=mock_config_manager)
+    token_manager = TokenManager(config=config_manager)
     client_id, client_secret = token_manager.get_credentials()
     assert client_id == "mocked_client_id"
     assert client_secret == "mocked_client_secret"
 
 
-def test_token_manager_initialization(mock_config_manager):
-    token_manager = TokenManager(config=mock_config_manager)
+def test_token_manager_initialization(config_manager):
+    token_manager = TokenManager(config=config_manager)
     assert token_manager.token.token == "mocked_token"
     assert token_manager.token.expires == datetime(2030, 1, 1, tzinfo=timezone.utc)
 
 
 @patch.object(ConfigManager, "set_credential")
-def test_save_token(mock_set_credential, mock_config_manager):
-    token_manager = TokenManager(config=mock_config_manager)
+def test_save_token(mock_set_credential, config_manager):
+    token_manager = TokenManager(config=config_manager)
     token = AccessToken(
         token="new_token", expires=datetime(2031, 1, 1, tzinfo=timezone.utc)
     )
@@ -90,3 +65,23 @@ def test_save_token(mock_set_credential, mock_config_manager):
         call("TOKEN_EXPIRATION", token.expires.isoformat()),
     ]
     mock_set_credential.assert_has_calls(expected_calls, any_order=True)
+
+
+def test_token_valid_true(token_manager):
+    # Make future_time timezone-aware by specifying tzinfo
+    future_time = datetime.utcnow().replace(tzinfo=timezone.utc) + timedelta(hours=1)
+    token_manager.token = AccessToken(token="dummy_token", expires=future_time)
+    assert token_manager.token_valid() is True
+
+
+def test_token_valid_false(token_manager):
+    # Make past_time timezone-aware by specifying tzinfo
+    past_time = datetime.utcnow().replace(tzinfo=timezone.utc) - timedelta(hours=1)
+    token_manager.token = AccessToken(token="dummy_token", expires=past_time)
+    assert token_manager.token_valid() is False
+
+
+def test_token_valid_no_expiration(token_manager):
+    past_time = datetime(1970, 1, 1, tzinfo=timezone.utc)
+    token_manager.token = AccessToken(token="dummy_token", expires=past_time)
+    assert token_manager.token_valid() is False
