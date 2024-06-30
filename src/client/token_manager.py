1fb50c739c2024081faa6a4e02bb5c2bc4b0f148
Andrew Speciale (@liquidz00) <aspeciale88@gmail.com>
2024-06-30 12:59:56 -0400

    Refactor token lifetime checks to default to `JamfClient` in place of unnecessary API call

--- src/client/token_manager.py
+++ src/client/token_manager.py
@@ -1,43 +1,67 @@
 from aiohttp import ClientSession, ClientResponseError
-from typing import AnyStr, Dict, Optional
+from typing import AnyStr, Optional
 from datetime import datetime, timedelta, timezone
 from src import logger
 from src.client.config_manager import ConfigManager
-from src.model.models import AccessToken
+from src.model.models import AccessToken, JamfClient
 
-logthis = logger.setup_child_logger("token_manager", __name__)
+logthis = logger.setup_child_logger("TokenManager", __name__)
 
 
 class TokenManager:
     def __init__(self, config: ConfigManager):
         self.config = config
-        self.token = self.load_token()
-
-    def load_token(self) -> AccessToken:
-        token = self.config.get_credential("TOKEN") or ""
-        expires = (
-            self.config.get_credential("TOKEN_EXPIRATION")
-            or datetime(1970, 1, 1, tzinfo=timezone.utc).isoformat()
-        )
-        return AccessToken(token=token, expires=expires)
+        self.jamf_client = self.config.attach_client()
+        if self.jamf_client:
+            self.token = self.jamf_client.token
+        else:
+            raise ValueError("Invalid JamfClient configuration detected!")
 
     def save_token(self, token: AccessToken):
         self.config.set_credential("TOKEN", token.token)
         self.config.set_credential("TOKEN_EXPIRATION", token.expires.isoformat())
         logthis.info("Bearer token and expiration updated in keyring")
+        self.jamf_client.token = token
 
     def token_valid(self) -> bool:
         return not self.token.is_expired
 
     def get_credentials(self):
-        client_id = self.config.get_credential("CLIENT_ID")
-        client_secret = self.config.get_credential("CLIENT_SECRET")
-        return client_id, client_secret
+        return self.jamf_client.client_id, self.jamf_client.client_secret
 
     def update_token(self, token_str: AnyStr, expires_in: int):
         expiration_time = datetime.now(timezone.utc) + timedelta(seconds=expires_in)
         self.token = AccessToken(token=token_str, expires=expiration_time)
         self.save_token(self.token)
+
+    def check_token_lifetime(self, client: Optional[JamfClient] = None) -> bool:
+        if client is None:
+            client = self.jamf_client
+
+        if not client.token:
+            logthis.error(f"No token found for JamfClient {client}")
+            return False
+
+        lifetime = client.token.seconds_remaining
+
+        if lifetime <= 0:
+            logthis.error("Token lifetime is invalid")
+            return False
+
+        minutes = lifetime / 60
+
+        if minutes < 1:
+            logthis.error("Token life time is less than 1 minute.")
+        elif 5 <= minutes <= 10:
+            # Throws warning if token lifetime is between 5-10 minutes
+            logthis.warning(
+                "Token lifetime is between 5-10 minutes, consider increasing duration."
+            )
+        else:
+            logthis.info(
+                f"Token lifetime is sfficient for {client.client_id}. Remaining Lifetime: {client.token.seconds_remaining}"
+            )
+            return True
 
     async def fetch_token(self) -> Optional[AccessToken]:
         client_id, client_secret = self.get_credentials()
@@ -50,7 +74,7 @@ class TokenManager:
             headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
             async with session.post(
-                url=f"{self.config.get_credential('URL')}/api/oauth/token",
+                url=f"{self.jamf_client.base_url}/api/oauth/token",
                 data=payload,
                 headers=headers,
             ) as resp:
@@ -74,79 +98,3 @@ class TokenManager:
                 except ClientResponseError as e:
                     logthis.error(f"Failed to fetch a token: {e}")
                     return None
-
-    async def check_token_lifetime(
-        self,
-        client_id: Optional[AnyStr] = None,
-        headers: Optional[Dict[str, str]] = None,
-    ) -> bool:
-        """
-        Ensures the bearer token lifetime is valid (longer than 1 minute, ideally above 10 minutes)
-
-        :param client_id: The client ID property to match. Defaults to client_id in keychain.
-        :type client_id: AnyStr
-        :param headers: The headers for the request.
-        :type headers: Dict[str, str]
-        :return: True if token lifetime is greater than 5 minutes
-        """
-        if client_id is None:
-            client_id = self.get_credentials()[0]
-        if headers is None:
-            headers = {"Accept": "application/json", "Authorization": f"Bearer {self.config.get_credential('TOKEN')}"}
-
-        async with ClientSession() as session:
-            url = f"{self.config.get_credential('URL')}/api/v1/api-integrations"
-            try:
-                async with session.get(url=url, headers=headers) as resp:
-                    resp.raise_for_status()
-                    response = await resp.json()
-            except ClientResponseError as e:
-                logthis.error(f"API error encountered: {e}")
-                return False
-
-        if not response:
-            logthis.error("Received empty dictionary fetching response.")
-            return False
-
-        results = response.get("results")
-        if not results:
-            logthis.error("Invalid response received from API call.")
-            return False
-
-        lifetime = None
-        for result in results:
-            if result.get("clientId") == client_id:
-                lifetime = result.get("accessTokenLifetimeSeconds")
-                logthis.info(f"Retrieved token lifetime for {client_id} successfully.")
-                break
-
-        if lifetime is None:
-            # Client ID not found
-            logthis.error(f"No matching Client ID found for {client_id}.")
-            return False
-
-        if lifetime <= 0:
-            logthis.error("Token lifetime is invalid.")
-            return False
-
-        # Calculate duration in different units
-        minutes = lifetime / 60
-        hours = minutes / 60
-        days = hours / 24
-        months = days / 30
-
-        # Throw error if duration of lifetime is less than 1 minute
-        if minutes < 1:
-            logthis.error("Token life time is less than 1 minute.")
-            return False
-        elif 5 <= minutes <= 10:
-            # Throws warning if token lifetime is between 5-10 minutes
-            logthis.warning(
-                "Token lifetime is between 5-10 minutes, consider increasing duration."
-            )
-        else:
-            # Lifetime duration logged otherwise
-            logthis.info(
-                f"Token lifetime: {minutes:.2f} minutes, {hours:.2f} hours, {days:.2f} days, {months:.2f} months."
-            )
-        return True
