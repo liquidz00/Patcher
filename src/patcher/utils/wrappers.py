a738ffb0087626b5c1b84c7d8a94c7346815461a
Andrew Speciale (@liquidz00) <aspeciale88@gmail.com>
2024-09-06 17:16:36 -0400

    Remove redundant code, refactor with existing methods from TokenManager class

--- src/patcher/utils/wrappers.py
+++ src/patcher/utils/wrappers.py
@@ -1,13 +1,9 @@
 from functools import wraps
 from typing import Any, Callable
 
-import aiohttp
 from pydantic import ValidationError
 
-from patcher.utils import exceptions, logger
-
-# Logging
-logthis = logger.setup_child_logger("wrappers", __name__)
+from ..utils import exceptions
 
 
 # Automatic checking/refreshing of AccessToken
@@ -40,44 +36,21 @@ def check_token(func: Callable) -> Any:
     async def wrapper(*args, **kwargs):
         instance = args[0]
         token_manager = instance.token_manager
-        config = instance.config
         log = instance.log
 
         try:
-            config.attach_client()
+            instance.config.attach_client()
+            await token_manager.ensure_valid_token()
+
         except ValidationError as e:
             log.error(f"Failed validation: {e}")
             raise
-
-        # Check if token is valid
-        log.debug("Checking bearer token validity")
-        if not token_manager.token_valid():
-            log.warning("Bearer token is invalid, attempting refresh...")
-            try:
-                token = await token_manager.fetch_token()
-                if token is None:
-                    log.error("Token refresh returned None")
-                    raise exceptions.TokenFetchError(reason="Token refresh returned None")
-                else:
-                    log.info("Token successfully refreshed.")
-            except aiohttp.ClientError as token_refresh_error:
-                log.error(f"Failed to refresh token: {token_refresh_error}")
-                raise exceptions.TokenFetchError(reason=str(token_refresh_error))
-        else:
-            log.debug("Bearer token passed validity checks.")
-
-        # Ensure token has proper lifetime duration
-        log.debug("Verifying token lifetime is greater than 5 minutes")
-        token_lifetime = token_manager.check_token_lifetime()
-        if token_lifetime:
-            log.debug("Token lifetime verified successfully.")
-        elif token_lifetime is False:
-            log.error(
-                "Bearer token lifetime is too short. Review the patcher Wiki for instructions to increase the token's lifetime."
-            )
-            raise exceptions.TokenLifetimeError(lifetime=instance.token.seconds_remaining)
-        else:
-            log.debug("Token lifetime is at least 5 minutes. Continuing...")
+        except exceptions.TokenFetchError as e:
+            log.error(f"Failed to refresh token: {e}")
+            raise
+        except exceptions.TokenLifetimeError as e:
+            log.error(f"Token lifetime insufficient: {e}")
+            raise
 
         # Proceed with original function
         return await func(*args, **kwargs)
