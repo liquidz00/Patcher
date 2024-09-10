c1dc6bbfd1f8ec70e43cd942a450786187d7e1d4
Andrew Speciale (@liquidz00) <aspeciale88@gmail.com>
2024-09-10 18:50:51 -0400

    Raise appropriate exceptions instead of returning None

--- src/patcher/client/api_client.py
+++ src/patcher/client/api_client.py
@@ -1,10 +1,9 @@
 import json
 from datetime import datetime
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Optional
 
-from ..models.jamf_client import ApiClientModel, ApiRoleModel
 from ..models.patch import PatchTitle
-from ..utils import logger
+from ..utils import logger, exceptions
 from ..utils.wrappers import check_token
 from . import BaseAPIClient
 from .config_manager import ConfigManager
@@ -52,18 +51,13 @@ class ApiClient(BaseAPIClient):
         :type utc_time_str: str
         :return: Formatted date string (e.g., "Aug 09 2023") or None if the input format is invalid.
         :rtype: Optional[str]
-
-        .. note::
-
-            This function is primarily used 'privately' by methods and classes and is not designed to be called explicitly.
-
         """
         try:
             utc_time = datetime.strptime(utc_time_str, "%Y-%m-%dT%H:%M:%S%z")
             return utc_time.strftime("%b %d %Y")
         except ValueError as e:
             self.log.error(f"Invalid time format provided. Details: {e}")
-            return None
+            raise
 
     @check_token
     async def get_policies(self) -> Optional[List]:
@@ -82,7 +76,7 @@ class ApiClient(BaseAPIClient):
             self.log.error(
                 f"Unexpected response format: expected a list, received {type(response)} instead."
             )
-            return None
+            raise TypeError(f"Unexpected response format: expected a list, received {type(response)} instead.")
 
         # Check if all elements in the list are dictionaries
         if not all(isinstance(item, dict) for item in response):
@@ -135,13 +129,13 @@ class ApiClient(BaseAPIClient):
         response = await self.fetch_json(url=url, headers=self.jamf_client.headers)
         if not response:
             self.log.error(f"Error fetching device IDs from {url}")
-            return None
+            raise exceptions.APIResponseError(f"Error fetching device IDs from {url}")
 
         devices = response.get("results")
 
         if not devices:
             self.log.error("Received empty data set when trying to obtain device IDs.")
-            return None
+            raise exceptions.SummaryFetchError("Received empty data set when trying to obtain device IDs.")
 
         self.log.info(f"Received {len(devices)} device IDs successfully.")
         return [device.get("id") for device in devices if device]
@@ -164,8 +158,8 @@ class ApiClient(BaseAPIClient):
         subsets = await self.fetch_batch(urls, headers=self.jamf_client.headers)
 
         if not subsets:
-            self.log.error("Received empty response obtaining device OS information.")
-            return None
+            self.log.error("Device OS API call returned an empty response.")
+            raise exceptions.APIResponseError("Device OS API call returned an empty response.")
 
         devices = [
             {
@@ -189,10 +183,17 @@ class ApiClient(BaseAPIClient):
                 or None on error.
         :rtype: Optional[List[Dict[str, str]]]
         """
+        # Call can be made directly as no additional headers or payloads need to be added
         command = ["/usr/bin/curl", "-s", "https://sofafeed.macadmins.io/v1/ios_data_feed.json"]
         result = await self.execute(command)
 
-        os_versions = result.get("OSVersions", [])
+        # Convert to JSON for proper parsing
+        result_json = json.loads(result)
+
+        # Get OS version from response
+        os_versions = result_json.get("OSVersions", [])
+
+        # Iterate over versions to obtain iOS 16 & iOS 17 datasets
         latest_versions = []
         for version in os_versions:
             version_info = version.get("Latest", {})
@@ -204,114 +205,3 @@ class ApiClient(BaseAPIClient):
                 }
             )
         return latest_versions
-
-    async def fetch_basic_token(
-        self, username: str, password: str, jamf_url: Optional[str] = None
-    ) -> Optional[str]:
-        """
-        Asynchronously retrieves a bearer token using basic authentication.
-
-        This method is intended for initial setup to obtain client credentials for API clients and roles.
-        It should not be used for regular token retrieval after setup.
-
-        :param username: Username of admin Jamf Pro account for authentication. Not permanently stored, only used for initial token retrieval.
-        :type username: str
-        :param password: Password of admin Jamf Pro account. Not permanently stored, only used for initial token retrieval.
-        :type password: str
-        :param jamf_url: Jamf Server URL (same as ``server_url`` in :mod:`patcher.models.jamf_client` class).
-        :type jamf_url: Optional[str]
-        :raises exceptions.TokenFetchError: If the call is unauthorized or unsuccessful.
-        :returns: True if the basic token was successfully retrieved, False if unauthorized (e.g., due to SSO).
-        :rtype: bool
-        """
-        jamf_url = jamf_url or self.jamf_url
-        token_url = f"{jamf_url}/api/v1/auth/token"
-        command = [
-            "/usr/bin/curl",
-            "-s",
-            "-u",
-            f"{username}:{password}",
-            "-H",
-            "accept: application/json",
-            "-X",
-            "POST",
-            token_url,
-        ]
-        async with self.semaphore:
-            response = await self.execute(command)
-            if response and "token" in response:
-                return response.get("token")
-            return None
-
-    async def create_roles(self, token: str) -> bool:
-        """
-        Creates the necessary API roles using the provided bearer token.
-
-        :param token: The bearer token to use for authentication. Defaults to the stored token if not provided.
-        :type token: Optional[str]
-        :return: True if roles were successfully created, False otherwise.
-        :rtype: bool
-        """
-        role = ApiRoleModel()
-        payload = json.dumps(
-            {
-                "displayName": role.display_name,
-                "privileges": role.privileges,
-            }
-        )
-        role_url = f"{self.jamf_url}/api/v1/api-roles"
-        headers = {
-            "accept": "application/json",
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {token}",
-        }
-        header_string = " ".join([f"-H '{k}: {v}'" for k, v in headers.items()])
-        command = ["/usr/bin/curl", "-s", header_string, "-d", payload, "-X", "POST", role_url]
-        response = await self.execute(command)
-        return response is not None
-
-    async def create_client(self, token: str) -> Optional[Tuple[str, str]]:
-        """
-        Creates an API client and retrieves its client ID and client secret.
-
-        This method uses the provided bearer token to create a new API client in the Jamf server.
-
-        :param token: The bearer token to use for authentication. Defaults to the stored token if not provided.
-        :type token: Optional[str]
-        :return: A tuple containing the client ID and client secret.
-        :rtype: Optional[Tuple[str, str]]
-        """
-        client = ApiClientModel()
-        client_url = f"{self.jamf_url}/api/v1/api-integrations"
-        payload = json.dumps(
-            {
-                "authorizationScopes": client.auth_scopes,
-                "displayName": client.display_name,
-                "enabled": client.enabled,
-                "accessTokenLifetimeSeconds": client.token_lifetime,  # 30 minutes in seconds
-            }
-        )
-        headers = {
-            "accept": "application/json",
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {token}",
-        }
-        header_string = " ".join([f"-H '{k}: {v}'" for k, v in headers.items()])
-        command = ["/usr/bin/curl", "-s", header_string, "-d", payload, "-X", "POST", client_url]
-
-        response = await self.execute(command)
-        if not response or "clientId" not in response:
-            return None
-
-        client_id = response.get("clientId")
-        integration_id = response.get("id")
-
-        secret_url = f"{self.jamf_url}/api/v1/api-integrations/{integration_id}/client-credentials"
-        secret_command = ["/usr/bin/curl", "-s", header_string, "-X", "POST", secret_url]
-        secret_response = await self.execute(secret_command)
-
-        if not secret_response or "clientSecret" not in secret_response:
-            return None
-
-        client_secret = secret_response.get("clientSecret")
-        return client_id, client_secret
