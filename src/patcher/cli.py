e39eb188885d4a3a6607b577b8375389fc5a9d90
Andrew Speciale (@liquidz00) <aspeciale88@gmail.com>
2024-12-28 15:45:18 -0500

    Refactor into three separate commands; `reset`, `export`, and `analyze`.

--- src/patcher/cli.py
+++ src/patcher/cli.py
@@ -1,9 +1,12 @@
 import asyncio
-from typing import Optional
+import sys
+from pathlib import Path
+from typing import Optional, Union
 
 import asyncclick as click
 
 from .__about__ import __version__
+from .client.analyze import Analyzer, FilterCriteria
 from .client.api_client import ApiClient
 from .client.config_manager import ConfigManager
 from .client.report_manager import ReportManager
@@ -13,7 +16,8 @@ from .client.ui_manager import UIConfigManager
 from .models.reports.excel_report import ExcelReport
 from .models.reports.pdf_report import PDFReport
 from .utils.animation import Animation
-from .utils.logger import LogMe
+from .utils.exceptions import PatcherError
+from .utils.logger import LogMe, PatcherLog
 
 DATE_FORMATS = {
     "Month-Year": "%B %Y",  # April 2024
@@ -23,21 +27,157 @@ DATE_FORMATS = {
     "Full": "%A %B %d %Y",  # Thursday September 26 2013
 }
 
+sys.excepthook = PatcherLog.custom_excepthook  # Log unhandled exceptions
 
-@click.command()
+
+def setup_logging(debug: bool) -> None:
+    """Configures global logging based on the debug flag."""
+    PatcherLog.setup_logger(debug=debug)
+
+
+# Context settings to enable both ``-h`` and ``--help`` for help output
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+
+
+# Entry
+@click.group(context_settings=CONTEXT_SETTINGS)
 @click.version_option(version=__version__)
+@click.option("--debug", "-x", is_flag=True, help="Enable debug logging.")
+@click.pass_context
+async def cli(ctx: click.Context, debug: bool) -> None:
+    """
+    Main CLI entry point for Patcher.
+
+    Visit our project documentation for full details: https://patcher.liquidzoo.io.
+
+    \b
+    Exit Codes:
+        0   Success
+        1   General error (e.g., PatcherError or user-facing issue)
+        2   Unhandled exception
+        130 KeyboardInterrupt (Ctrl+C)
+    """
+    setup_logging(debug)
+
+    # Instantiate classes, store in context
+    ctx.obj = {
+        "debug": debug,
+        "animation": Animation(enable_animation=not debug),
+        "log": LogMe(__name__),
+        "config": ConfigManager(),
+        "ui_config": UIConfigManager(),
+    }
+
+    setup = Setup(ctx.obj.get("config"), ctx.obj.get("ui_config"))
+    ctx.obj["setup"] = setup
+
+    # Check Setup completion
+    async with ctx.obj.get("animation").error_handling():
+        if not setup.completed:
+            await setup.start(animator=ctx.obj.get("animation"))
+            click.echo(click.style(text="Setup has completed successfully!", fg="green", bold=True))
+            click.echo(
+                "Patcher is now ready for use. You can use the --help flag to view available options."
+            )
+            click.echo("For more information, visit the project docs: https://patcher.liquidzoo.io")
+            # Exit to avoid running a command
+            ctx.exit(0)
+
+
+# Reset
+@cli.command()
+@click.argument(
+    "kind",
+    type=click.Choice(["full", "UI", "creds"], case_sensitive=False),
+    required=True,
+)
+@click.option(
+    "--credential",
+    "-c",
+    type=click.Choice(["url", "client_id", "client_secret"], case_sensitive=False),
+    help="Specify which credential to reset: URL, Client ID, or Client Secret. Defaults to all if not provided.",
+)
+@click.pass_context
+async def reset(ctx: click.Context, kind: str, credential: Optional[str]) -> None:
+    """
+    Resets configurations based on the specified kind ("full", "UI", "creds").
+
+    \f
+    **Options**:
+
+    - ``"full"``: Resets credentials, UI elements, and property list file. Subsequently triggers :class:`~patcher.client.setup.Setup` to start setup.
+    - ``"UI"``: Resets UI elements of PDF reports (header & footer text, custom font and optional logo).
+    - ``"creds"``: Resets credentials stored in Keychain. Useful for testing Patcher in a non-production environment first. Allows specifying which credential to reset using the ``--credential`` option.
+
+    :param ctx: The context object, providing access to shared state between commands.
+    :type ctx: click.Context
+    :param kind: Specifies the type of reset to perform.
+    :type kind: str
+    :param credential: The specific credential to reset when performing credentials reset. Defaults to all credentials if none specified.
+    :type credential: Optional[str]
+    """
+    log = ctx.obj.get("log")
+    config = ctx.obj.get("config")
+    ui_config = ctx.obj.get("ui_config")
+    setup = ctx.obj.get("setup")
+    animation = ctx.obj.get("animation")
+
+    async with animation.error_handling():
+        if kind.lower() == "full":
+            # Perform full reset_config here
+            log.info("Performing full reset...")
+            config.reset_config()
+            ui_config.reset_config()
+            setup.reset_setup()
+            async with animation.error_handling():
+                await setup.start(animation)
+        elif kind.lower() == "ui":
+            log.info("Resetting UI elements...")
+            if ui_config.reset_config():
+                # Only prompt to setup UI if reset_config was successful
+                ui_config.setup_ui()
+        elif kind.lower() == "creds":
+            log.info(f"Resetting credentials... (specific: {credential if credential else 'all'})")
+            # Keyring automatically overwrites existing passwords if key and service_name are the same.
+            # This allows us to just call the set_credential method instead of having to delete existing
+            # entries first.
+            match credential:
+                case "url":
+                    new_url = click.prompt("Enter your Jamf Pro URL")
+                    config.set_credential("URL", new_url)
+                case "client_id":
+                    new_client_id = click.prompt("Enter your API Client ID")
+                    config.set_credential("CLIENT_ID", new_client_id)
+                case "client_secret":
+                    new_client_secret = click.prompt("Enter your API Client Secret")
+                    config.set_credential("CLIENT_SECRET", new_client_secret)
+                case None:
+                    log.info("Attempting to delete all credentials from keychain...")
+                    cred_map = {
+                        "URL": click.prompt("Enter your Jamf Pro URL"),
+                        "CLIENT_ID": click.prompt("Enter your API Client ID"),
+                        "CLIENT_SECRET": click.prompt("Enter your API Client Secret"),
+                    }
+                    for k, v in cred_map.items():
+                        config.set_credential(k, v)
+
+    click.echo(click.style("✅ Reset finished successfully.", fg="green", bold=True))
+
+
+# Export
+@cli.command()
 @click.option(
     "--path",
     "-p",
     type=click.Path(),
-    required=False,  # Defaulting to false in favor of `--reset`
-    help="Path to save the report(s)",
+    required=True,
+    help="File path to save the generated report(s).",
 )
 @click.option(
     "--pdf",
     "-f",
     is_flag=True,
-    help="Generate a PDF report along with Excel spreadsheet",
+    help="Generate a PDF report along with Excel spreadsheet.",
 )
 @click.option(
     "--sort",
@@ -50,7 +190,7 @@ DATE_FORMATS = {
     "--omit",
     "-o",
     is_flag=True,
-    help="Omit software titles with patches released in last 48 hours",
+    help="Omit software titles with patches released in last 48 hours.",
 )
 @click.option(
     "--date-format",
@@ -71,22 +211,8 @@ DATE_FORMATS = {
     default=5,
     help="Set the maximum concurrency level for API calls.",
 )
-@click.option(
-    "--debug",
-    "-x",
-    is_flag=True,
-    default=False,
-    help="Enable debug logging to see detailed debug messages.",
-)
-@click.option(
-    "--reset",
-    "-r",
-    is_flag=True,
-    default=False,
-    help="Resets the setup process and triggers the setup assistant again.",
-)
 @click.pass_context
-async def main(
+async def export(
     ctx: click.Context,
     path: str,
     pdf: bool,
@@ -95,47 +221,165 @@ async def main(
     date_format: str,
     ios: bool,
     concurrency: int,
-    debug: bool,
-    reset: bool,
 ) -> None:
-    if not ctx.params["reset"] and not ctx.params["path"]:
-        raise click.UsageError("The --path option is required unless --reset is specified.")
+    """
+    Collects patch management data from Jamf API calls and exports data to Excel and optional
+    PDF formats.
+    \f
 
-    log = LogMe(__name__, debug=debug)
-    animation = Animation(enable_animation=not debug)
+    .. seealso::
 
+        - :meth:`~patcher.client.report_manager.ReportManager.process_reports`
+        - :meth:`~patcher.client.__init__.BaseAPIClient.concurrency`
+        - :ref: `usage`
+
+    :param ctx: The context object, providing access to shared state between commands.
+    :type ctx: click.Context
+    :param path: The path to save the generated report(s).
+    :type path: str
+    :param pdf: Specifies whether or not to generate a PDF document in addition to Excel spreadsheet.
+    :type pdf: bool
+    :param sort: Sort the patch reports by specifying a column.
+    :type sort: Optional[str]
+    :param omit: Omit software titles with patches released in last 48 hours.
+    :type omit: bool
+    :param date_format: Specify the date format for the PDF header. Default is "%B %d %Y" (Month Day Year).
+    :type date_format: str
+    :param ios: If passed, includes iOS device data in exported reports.
+    :type ios: bool
+    :param concurrency: The maximum number of API requests that can be sent at once. Defaults to 5.
+    :type concurrency: int
+    """
     config = ConfigManager()
     ui_config = UIConfigManager()
 
-    setup = Setup(config=config, ui_config=ui_config)
+    api_client = ApiClient(config, concurrency)
+    token_manager = TokenManager(config)
+    excel_report = ExcelReport()
+    pdf_report = PDFReport(ui_config)
 
-    async with animation.error_handling():
-        if not setup.completed:
-            await setup.prompt_method(animator=animation)
-            click.echo(click.style(text="Setup has completed successfully!", fg="green", bold=True))
-            click.echo("Patcher is now ready for use.")
-            click.echo("You can use the --help flag to view available options.")
-            click.echo("For more information, visit the project docs: https://patcher.liquidzoo.io")
-            return
-        elif reset:
-            await animation.update_msg("Resetting elements...")
-            await setup.reset()
-            click.echo(click.style(text="Reset has completed as expected!", fg="green", bold=True))
-            return
+    patcher = ReportManager(
+        config,
+        token_manager,
+        api_client,
+        excel_report,
+        pdf_report,
+        ui_config,
+        debug=ctx.obj.get("debug"),
+    )
 
-        api_client = ApiClient(config, concurrency)
-        token_manager = TokenManager(config)
-        excel_report = ExcelReport()
-        pdf_report = PDFReport(ui_config)
-        api_client.concurrency = concurrency
+    actual_format = DATE_FORMATS[date_format]
 
-        patcher = ReportManager(
-            config, token_manager, api_client, excel_report, pdf_report, ui_config, debug
+    # Not wrapping in animation error_handling in favor of existence on process_reports method
+    await patcher.process_reports(path, pdf, sort, omit, ios, actual_format)
+
+
+# Analyze
+@cli.command()
+@click.argument("excel_file", type=click.Path(exists=True))
+@click.option("--criteria", "-c", required=True, help="Filter criteria (e.g., 'most-installed').")
+@click.option(
+    "--threshold", "-t", type=float, default=70.0, help="Threshold percentage for filtering."
+)
+@click.option("--top-n", "-n", type=int, help="Limit the number of results displayed.")
+@click.option("--summary", "-s", is_flag=True, help="Generate summary analysis for output.")
+@click.option("--output-dir", "-o", type=click.Path(), help="Directory to save summary.")
+@click.pass_context
+async def analyze(
+    ctx: click.Context,
+    excel_file: str,
+    criteria: str,
+    threshold: float,
+    top_n: int = None,
+    summary: bool = False,
+    output_dir: Union[str, Path] = None,
+) -> None:
+    """
+    Analyzes an Excel file with patch management data and outputs analyzed results.
+
+    \f
+
+    :param ctx: Context object for shared state across CLI commands.
+    :type ctx: click.Context
+    :param excel_file: Path to the Excel file containing patch management data.
+    :type excel_file: str
+    :param threshold: Filters patches below the specified completion percentage.
+    :type threshold: float
+    :param criteria: Specifies the criteria for filtering patches.
+    :type criteria: str
+    :param top_n: Number of top entries to display based on the criteria.
+    :type top_n: int
+    :param summary: Flag to generate a summary file.
+    :type summary: bool
+    :param output_dir: Path to save generated summary, only if `--summary` flag passed.
+    :type output_dir: Union[str, Path]
+    """
+    if summary and not output_dir:
+        click.echo(
+            click.style(
+                "The --summary flag requires a valid directory specified with --output-dir.",
+                fg="yellow",
+            ),
+            err=True,
         )
+        return
 
-        actual_format = DATE_FORMATS[date_format]
-        await patcher.process_reports(path, pdf, sort, omit, ios, actual_format)
+    animation = ctx.obj.get("animation")
+    async with animation.error_handling():
+        analyzer = Analyzer(excel_file)
+        filter_criteria = FilterCriteria.from_cli(criteria)
+        filtered_titles = analyzer.filter_titles(filter_criteria, threshold, top_n)
+        if len(filtered_titles) == 0:
+            await animation.stop()
+            click.echo(f"No PatchTitle objects meet criteria {criteria}")
+            return
+
+        table_data = [
+            [
+                t.title,
+                t.released,
+                t.hosts_patched,
+                t.missing_patch,
+                t.latest_version,
+                t.completion_percent,
+                t.total_hosts,
+            ]
+            for t in filtered_titles
+        ]
+        headers = [
+            "Title",
+            "Released",
+            "Hosts Patched",
+            "Missing Patch",
+            "Latest Version",
+            "Completion %",
+            "Total Hosts",
+        ]
+        formatted_table = analyzer.format_table(table_data, headers)
+
+    click.echo(formatted_table)
+
+    if summary:
+        output_path = Path(output_dir) / "analysis_summary.txt"
+        try:
+            with open(output_path, "w") as f:
+                f.write(formatted_table)
+        except (OSError, PermissionError, FileNotFoundError) as exc:
+            raise PatcherError("Unable to save summary report as expected.", path=output_path, error_msg=str(exc))
+        click.echo(click.style(f"✅ Summary saved to {output_path}", fg="green", bold=True))
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(cli())
+    except PatcherError as e:
+        click.echo(click.style(f"❌Error: {str(e)}", fg="red", bold=True), err=True)
+        click.echo(
+            f"💡 For more details, please check the log file at: {PatcherLog.LOG_FILE}",
+            err=True,
+        )
+        sys.exit(1)
+    except Exception as e:
+        # Delegate to sys.excepthook
+        sys.excepthook(type(e), e, e.__traceback__)
+        sys.exit(2)
