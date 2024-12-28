b706db4df2f2a9c5b4975935fcf45362c7891243
Andrew Speciale (@liquidz00) <aspeciale88@gmail.com>
2024-12-28 13:51:51 -0500

    ✅

--- src/patcher/client/report_manager.py
+++ src/patcher/client/report_manager.py
@@ -370,4 +370,6 @@ class ReportManager:
 
     def _success(self, report_count: int, reports_dir: str) -> None:
         self.log.info(f"{report_count} patch reports saved successfully to {reports_dir}.")
-        click.echo(click.style(f"\r✅ Success! Reports saved to {reports_dir}", bold=True, fg="green"))
+        click.echo(
+            click.style(f"\r✅ Success! Reports saved to {reports_dir}", bold=True, fg="green")
+        )
